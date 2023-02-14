c5d1e52b57fa3b91284e4e29b6f161124b3e5b4c
IntissarJ <intissar_jamal@outlook.com>
2023-02-14 12:45:58 +0100

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by Intissar Jamal on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlwaysNote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AlwaysNote
+//
+//  Created by Intissar Jamal on 14/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
